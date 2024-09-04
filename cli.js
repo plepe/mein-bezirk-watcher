7527b05182c00c1288e80781cbbe8dfb91923cd6
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2024-09-04 09:36:38 +0200

    Parse title and id from links; merge and print results

--- cli.js
+++ cli.js
@@ -6,12 +6,25 @@ import parsePage from './src/parsePage.js'
 
 const config = yaml.load(fs.readFileSync('conf.yaml'))
 
+let result = {}
 async.each(config.tags, (tag, done) => {
   fetch('https://meinbezirk.at/tag/' + tag)
     .then(req => req.text())
     .then(body => parsePage(body))
     .then(list => {
-      console.log(list)
+      list.forEach(item => {
+        if (!(item.id in result)) {
+          result[item.id] = item
+        }
+      })
+
       done()
     })
+}, (err) => {
+  if (err) {
+    console.error(err)
+  }
+
+  result = Object.values(result)
+  console.log(JSON.stringify(result, null, '   '))
 })
--- src/parsePage.js
+++ src/parsePage.js
@@ -1,5 +1,20 @@
+import { JSDOM } from 'jsdom'
+
 export default function parsePage (body) {
   return new Promise((resolve, reject) => {
-    resolve('foo')
+    const document = new JSDOM(body).window.document
+    const articles = document.body.querySelectorAll('#content-main article')
+
+    const result = Array.from(articles).map(article => {
+      const item = {
+        id: article.getAttribute('data-article-id'),
+        href: article.querySelector('a').getAttribute('href'),
+        title: (article.querySelector('h3 .kicker') ? article.querySelector('h3 .kicker').textContent.trim() + ': ' : '') + article.querySelector('h3 a').lastChild.textContent.trim()
+      }
+
+      return item
+    })
+
+    resolve(result)
   })
 }
