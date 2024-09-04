285ca30250852f95638d70d091a3b35bdc1f3dee
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2024-09-04 08:56:27 +0200

    Load pages and call parse function

--- cli.js
+++ cli.js
@@ -2,10 +2,16 @@
 import fs from 'fs'
 import yaml from 'js-yaml'
 import async from 'async'
+import parsePage from './src/parsePage.js'
 
 const config = yaml.load(fs.readFileSync('conf.yaml'))
 
 async.each(config.tags, (tag, done) => {
-  console.log(tag)
-  done()
+  fetch('https://meinbezirk.at/tag/' + tag)
+    .then(req => req.text())
+    .then(body => parsePage(body))
+    .then(list => {
+      console.log(list)
+      done()
+    })
 })
---
+++ src/parsePage.js
@@ -0,0 +1,5 @@
+export default function parsePage (body) {
+  return new Promise((resolve, reject) => {
+    resolve('foo')
+  })
+}
