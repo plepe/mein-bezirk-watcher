5dc3495ab6988cea5034e7a719544caba5891f08
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-01-04 16:38:57 +0100

    Website: first working version

--- src/app.js
+++ src/app.js
@@ -1,2 +1,49 @@
+let form
+let result
+
 window.onload = () => {
+  result = document.querySelector('#result')
+  form = document.querySelector('form')
+  form.onchange = loadResults
+  loadResults()
+}
+
+function loadResults () {
+  console.log(form.elements)
+
+  const parameter = {
+    tags: form.elements.tags.value,
+    loc: form.elements.loc.value
+  }
+
+  result.innerHTML = ''
+  if (parameter.tags === '') {
+    return
+  }
+
+  let url = '.?tags=' + encodeURIComponent(parameter.tags)
+  if (parameter.loc) {
+    url += '&loc=' + encodeURIComponent(parameter.loc)
+  }
+
+  fetch(url)
+    .then(req => req.json())
+    .then(data => {
+      data.forEach(entry => {
+        showEntry(entry)
+      })
+    })
+}
+
+function showEntry (entry) {
+  const node = document.createElement('li')
+
+  const a = document.createElement('a')
+  a.href = entry.href
+  a.target = '_blank'
+  a.appendChild(document.createTextNode(entry.title))
+
+  node.appendChild(a)
+
+  result.appendChild(node)
 }
